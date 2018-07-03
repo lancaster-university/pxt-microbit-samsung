94c0ad3cf19973e783baba6fd262e5c2b4057dab
Brian Mitchell <b.mitchell@ws-1809.seri.co.uk>
2018-07-03 13:48:35 +0100

    Added ISS package

---
+++ libs/iss/shims.d.ts
@@ -0,0 +1,12 @@
+// Auto-generated. Do not edit.
+
+
+    /**
+     * share
+     * use
+     */
+    //% color=#99ccff weight=100 icon="\uf197" block="ISS"
+declare namespace iss {
+}
+
+// Auto-generated. Do not edit. Really.
