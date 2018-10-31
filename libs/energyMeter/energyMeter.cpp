2a8b423bfbdee1fabe523f12c2d547edb40b8176
James Devine <j.devine@lancaster.ac.uk>
2018-10-31 16:29:36 +0000

    rename energymeter package and fix compile

--- libs/energyMeter/energyMeter.cpp
+++ libs/energyMeter/energyMeter.cpp
@@ -43,24 +43,31 @@ namespace energymeter {
         registerWithDal(MICROBIT_ID_ENERGY_MONITOR, (state == ApplianceState::on) ? MICROBIT_ENERGY_MONITOR_EVT_POWER_ON : MICROBIT_ENERGY_MONITOR_EVT_POWER_OFF, body);
     }
 
-    //
+    //%
     void sendEnergyMeterReading(ApplianceType type, StringData* name)
     {
         init();
         energymeterService.sendEnergyLevel(ManagedString(name), uBit.energyMonitor.getEnergyUsage(), (int)type);
     }
 
-    //
+    //%
     int getElectricalPowerStatus()
     {
         init();
         return (uBit.energyMonitor.isElectricalPowerOn()) ? 1 : 0;
     }
 
-    //
+    //%
     int getEnergyUsage()
     {
         init();
         return (uBit.energyMonitor.getEnergyUsage()) ? 1 : 0;
+    }
+
+    //%
+    void calibrateEnergyMeter()
+    {
+        init();
+        uBit.energyMonitor.calibrate();
     }
 };
--- libs/energyMeter/energyMeter.ts
+++ libs/energyMeter/energyMeter.ts
@@ -8,7 +8,7 @@ namespace energymeter {
     //% help=none
     //% weight=96
     //% blockId=send_meter_reading block="send power level (watts) of my|%APPLIANCE_TYPE| called|%name to the cloud" blockGap=8
-    //% shim=energyMeter::sendEnergyMeterReading
+    //% shim=energymeter::sendEnergyMeterReading
     export function sendEnergyMeterReading(type:ApplianceType, name:string)
     {
         let p = 0;
@@ -20,6 +20,7 @@ namespace energymeter {
     //% help=none
     //% weight=96
     //% blockId=calibrate_energy_meter block="calibrate energy meter" blockGap=8
+    //% shim=energymeter::calibrateEnergyMeter
     export function calibrateEnergyMeter()
     {
         let p = 0;
@@ -30,7 +31,7 @@ namespace energymeter {
      */
     //% blockId=get_electrical_power_state block="electrical power status" blockGap=8
     //% weight=99
-    //% shim=energyMeter::getElectricalPowerStatus
+    //% shim=energymeter::getElectricalPowerStatus
     export function getElectricalPowerStatus() : number
     {
         return 0
@@ -41,7 +42,7 @@ namespace energymeter {
      */
     //% blockId=get_electrical_power_usage block="get energy consumption (watts)" blockGap=8
     //% weight=99
-    //% shim=energyMeter::getEnergyUsage
+    //% shim=energymeter::getEnergyUsage
     export function getEnergyUsage() : number
     {
         return Math.random(50);
